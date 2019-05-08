b79ad23eec902c1fc3e11dfc68376925709e95af
Mellisa Udhayananondh <udhayananondh@gmail.com>
2019-05-08 10:05:32 -0500

    install bootstrap add rideshare page

---
+++ src/views/Rideshare.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="rideshare">
+    <h1>does this show?</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  name: "rideshare"
+};
+</script>
